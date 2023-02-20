65c5f92830363d33660a7d7a90e7cee74ace6d79
git-santanu <sddutta90@gmail.com>
2023-02-20 23:35:31 +0530

    sdBigOO

--- O(test).js
+++ O(test).js
@@ -4,15 +4,16 @@ const anotherFunction = () => {
   console.log("object");
 };
 function funChallenge(input) {
-  let a = 10;
+  let a = 10; //O(1)
   console.log(a);
-  a = 50 + 3;
+  a = 50 + 3;//O(1)
   console.log(a);
-  for (let i = 0; i < input.length; i++) {
-    anotherFunction();
-    let stranger = true;
-    a++;
+  for (let i = 0; i < input.length; i++) {//0(n)
+    anotherFunction(); //0(n)
+    let stranger = true;//0(n)
+    a++;//0(n)
   }
-  console.log(a);
+  console.log(a);//O(1)
 }
 funChallenge(input);
+// Time complexity--> O(3+4n)
