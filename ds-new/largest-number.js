d5b527d3f14d70c21d67d9c522bb3440675d34e3
git-santanu <sddutta90@gmail.com>
2025-06-08 22:40:33 +0530

    negative & largest

---
+++ ds-new/largest-number.js
@@ -0,0 +1,15 @@
+const arr = [-1, 3, 10, 50, 9, 34, 25, 100, 255];
+
+const findLargest = (arr) => {
+  let max_num = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max_num) {
+      max_num = arr[i];
+    }
+  }
+  return max_num;
+};
+
+
+const res = findLargest(arr);
+console.log(res)
