82e8e1a30ee183f438137f7da53140343ce801ac
git-santanu <sddutta90@gmail.com>
2025-06-08 22:59:29 +0530

    smallest & largest

---
+++ ds-new/largest-smallest-number.js
@@ -0,0 +1,28 @@
+const arr = [1, 3, 10, 50, 9, 34, 25, 100, 255];
+
+const findLargest = (arr) => {
+  let max_num = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max_num) {
+      max_num = arr[i];
+    }
+  }
+  return max_num;
+};
+
+
+const res = findLargest(arr);
+console.log(res)
+
+const findSmallest = (arr) => {
+    let small_num = Infinity
+    for(let i= 0; i<arr.length; i++) {
+        if(arr[i] < small_num) {
+            small_num = arr[i]
+        }
+    }
+    return small_num
+}
+
+const res1 = findSmallest(arr);
+console.log(res1)
