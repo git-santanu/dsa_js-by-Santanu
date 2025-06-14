a9035bad01f53473fb3eaf7e0471036adc608214
git-santanu <sddutta90@gmail.com>
2025-06-14 22:28:54 +0530

    second largest

--- ds-new/max-number-arr.js
+++ ds-new/max-number-arr.js
@@ -20,3 +20,18 @@ for(let i =0; i < arr.length; i++) {
 return min;
 }
 console.log(findMinmumNumber(arr));
+
+const findSecondLargestNumber = (arr) => {
+    let largest = -Infinity;
+    let secondLargest = -Infinity
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] > largest) {
+            secondLargest = largest
+            largest = arr[i]
+        } else if (arr[i] < largest) {
+            largest = arr[i]
+        }
+    }
+    return secondLargest
+}
+console.log(findSecondLargestNumber(arr))
