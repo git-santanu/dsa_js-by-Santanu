173e31eca0bdde440b8100c5ebba0babee326817
git-santanu <sddutta90@gmail.com>
2025-07-05 19:26:34 +0530

    smallest & largest

--- ds-new/palindrome.js
+++ ds-new/palindrome.js
@@ -1,30 +1,29 @@
-// check palindrome 
+// check palindrome
 // pseudo code
 
 // st >>
 // end <<
-// st < end : 
+// st < end :
 // if (!isAlphaNumeric(st)) st ++; continue;
 // if (!isAlphaNumeric(end)) end --; continue;
 // if (lowerCase(st) !== lowerCase(end)) {
 //     return false;
 // }
-// st++; 
+// st++;
 // end --;
 // return true // outside the loop
 
 // pseudo code
 
-
 const isAlphaNumeric = (string) => {
-    if(
-        (string >= '0' && string <= '9') ||
-        ((string).toLowerCase() >= 'a' && (string).toLowerCase() <= 'z')
-    ) {
-        return true
-    }
-    return false
-}
+  if (
+    (string >= "0" && string <= "9") ||
+    (string.toLowerCase() >= "a" && string.toLowerCase() <= "z")
+  ) {
+    return true;
+  }
+  return false;
+};
 
 const isPalinDrome = (string) => {
   let st = 0;
@@ -46,4 +45,19 @@ const isPalinDrome = (string) => {
   }
   return true;
 };
-console.log(isPalinDrome('1@2%1'))
+console.log(isPalinDrome("1@2%1"));
+
+const isNumberPalinDrome = (num) => {
+  if (typeof num === 'string') return 'please provide number!'
+  const original_num = num;
+  let rev = 0;
+  let last = 0;
+  while (num > 0) {
+    last = num%10;
+    rev = (rev*10) + last;
+    num = Math.floor(num/10);
+  }
+  return rev === original_num;
+};
+const res = isNumberPalinDrome('');
+console.log(res)
