6ce95647c11cfaf26cae763ea2cfbeb943966975
git-santanu <sddutta90@gmail.com>
2025-07-12 17:21:10 +0530

    reverse integer with MAX & MIN integer

--- ds-new/reverse-integer.js
+++ ds-new/reverse-integer.js
@@ -1,6 +1,10 @@
+const isInt32 =(num)=> {
+    const MAX_INT = 2147483647; // 2^31 - 1
+    const MIN_INT = -2147483648; // -2^31
+    return Number.isInteger(num) && num >=MIN_INT && num<=MAX_INT
+}
 const reverseInteger = (num) => {
-  let isNagetive = false;
-  if (num < 0) isNagetive = true;
+  let isNagetive = num < 0;
   num = Math.abs(num);
   let getLastDigit = 0;
   let prevOfLastDigit = 0;
@@ -9,7 +13,8 @@ const reverseInteger = (num) => {
     prevOfLastDigit = prevOfLastDigit * 10 + getLastDigit;
     num = Math.floor(num / 10);
   }
+  if(!isInt32(isNagetive ? -prevOfLastDigit: prevOfLastDigit )) return 0
   return isNagetive ? -prevOfLastDigit : prevOfLastDigit;
 };
-const res = reverseInteger(-321);
+const res = reverseInteger(123);
 console.log(res);
