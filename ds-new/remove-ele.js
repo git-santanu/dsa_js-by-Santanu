40e31769c5aca38bdea5971c480b8f456502e6b7
git-santanu <sddutta90@gmail.com>
2025-07-19 19:59:04 +0530

    Remove Element

---
+++ ds-new/remove-ele.js
@@ -0,0 +1,18 @@
+const removeElement = (n) => {
+  let x = 0;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] > n[x]) {
+      x = x + 1;
+      n[x] = n[i];
+    }
+  }
+  let unique = [];
+  for (let j = 0; j <= x; j++) {
+    unique[j] = n[j];
+  }
+  return { no_unique_elements:x+1, unique };
+};
+const arr = [1, 2, 2, 3, 3, 4, 4, 4, 4, 5];
+const res = removeElement(arr);
+console.log(res);
+//expected output: 5, [1,2,3,4,5]
